93cf66681f5dd60f136c522ad0c79ab385db6ffc
DKOK01 <aymansadik2005@gmail.com>
2025-03-20 17:32:24 +0000

    .

---
+++ Makefile
@@ -0,0 +1,38 @@
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+SERVER = server
+CLIENT = client
+
+LIBFT_DIR = libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+SRCS_SERVER = server.c
+SRCS_CLIENT = client.c
+
+OBJS_SERVER = $(SRCS_SERVER:.c=.o)
+OBJS_CLIENT = $(SRCS_CLIENT:.c=.o)
+
+all: $(LIBFT) $(SERVER) $(CLIENT)
+
+$(LIBFT):
+	make -C $(LIBFT_DIR)
+
+$(SERVER): $(OBJS_SERVER) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJS_SERVER) -L$(LIBFT_DIR) -lft -o $(SERVER)
+
+$(CLIENT): $(OBJS_CLIENT) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJS_CLIENT) -L$(LIBFT_DIR) -lft -o $(CLIENT)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJS_SERVER) $(OBJS_CLIENT)
+	make clean -C $(LIBFT_DIR)
+
+fclean: clean
+	rm -f $(SERVER) $(CLIENT)
+	make fclean -C $(LIBFT_DIR)
+
+re: fclean all
---
+++ src/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aysadeq <aysadeq@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 20:12:02 by aysadeq           #+#    #+#             */
+/*   Updated: 2025/03/20 17:31:12 by aysadeq          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# include "../includes/libft/libft.h"
+
+#endif
